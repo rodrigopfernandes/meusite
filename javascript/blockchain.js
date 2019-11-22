68938e2b895bc3bdbee22eb5b825728d0238e7d7
rodrigopfernandes <56077942+rodrigopfernandes@users.noreply.github.com>
2019-11-21 21:06:12 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0x79cf9Ac7C7476D5e8D547Bd381A69E3f85Ab29c8";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
